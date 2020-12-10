e0ca49f8bcd3f668240f65ceac0768bd6000da28
Kristen Steele <ksteelephoto@gmail.com>
2020-12-09 20:22:14 -0600

    update index.js to route to BespokeHub.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
+import { BespokeHub } from "./components/BespokeHub"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <BespokeHub />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
