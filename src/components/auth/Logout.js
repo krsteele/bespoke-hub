444fd23816e1427b81d8c755e04d9a9f4366e307
Kristen Steele <ksteelephoto@gmail.com>
2020-12-10 14:39:21 -0600

    adds logout button and functionality

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -5,12 +5,13 @@ export const Logout = (props) => {
     const handleLogout = (e) => {
         e.preventDefault()
         localStorage.clear()
+        props.history.push("/login")
     } 
     
     return (
         <form onSubmit={handleLogout}>
             <fieldset>
-                <button type="logout">
+                <button type="submit">
                     Sign out
                 </button>
              </fieldset>
