da87027ae980ca0e61a65886b5fffe757f504d59
Kristen Steele <ksteelephoto@gmail.com>
2020-12-10 20:55:09 -0600

    adds list of all users at /people

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,8 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { ClientViewList } from "./client-views/ClientViewList"
 import { ProjectsList } from "./projects/ProjectsList"
+import { UserProvider } from "./users/UsersDataProvider"
+import { UsersList } from "./users/UsersList"
 
 
 export const ApplicationViews = () => {
@@ -10,6 +12,13 @@ export const ApplicationViews = () => {
             <Route exact path="/">
                 <ProjectsList />
             </Route>
+            <UserProvider>
+                <Route exact path="/people" render={
+                    props => {
+                        console.log("props from react router dom:", props)
+                        return <UsersList {...props} />}
+                    } />
+            </UserProvider>
             <Route exact path="/dashboards">
                 <ClientViewList />
             </Route>
