56250152d2245ba6b0fb71f9743a6761684889ce
Kristen Steele <ksteelephoto@gmail.com>
2020-12-12 15:46:23 -0600

    adds functionality to ProjectsList -- gets projects and maps to create link list of projects

--- src/components/projects/ProjectsList.js
+++ src/components/projects/ProjectsList.js
@@ -1,7 +1,34 @@
-import React from "react"
+import React, { useContext, useEffect } from "react"
+import { ProjectContext } from "./ProjectsDataProvider"
+import { Link } from "react-router-dom"
 
-export const ProjectsList = () => {
+export const ProjectsList = (props) => {
+    const { projects, getProjects } = useContext(ProjectContext)
+
+    useEffect(() => {
+        console.log("I'm ProjectsList initial render!")
+        getProjects()
+    }, [])
+
+    useEffect(() => {
+        console.log("I'm ProjectsList second render with data:", projects)
+    }, [projects])
+
     return (
-        <div>Hi, I'm the Projects List, which will be seen by the maker upon initial render!</div>
+        <div className="projects">
+            <h1>Projects</h1>
+            <button onClick={() => props.history.push("/projects/create")}>
+                Add New
+            </button>
+            <article className="projectsList">
+                {
+                    projects.map(project => {
+                        return <Link key={project.id} to={`/projects/${project.id}`}>
+                            <p>"{project.boatName}" — {project.boatLength}' {project.model} {project.year}</p>
+                        </Link>
+                    })
+                }
+            </article>
+        </div>
     )
 }
