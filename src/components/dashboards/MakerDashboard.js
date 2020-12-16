5f4fc7f4259c1369ca28b9c180b34a3463bf736d
Kristen Steele <ksteelephoto@gmail.com>
2020-12-16 08:17:58 -0600

    export basic MakerProjectDashboard function that calls UserDetail, ProjectDetail and TasksList

--- src/components/dashboards/MakerDashboard.js
+++ src/components/dashboards/MakerDashboard.js
@@ -1,3 +1,17 @@
 import React from "react"
+import { UserDetail } from "../users/UserDetail"
+import { ProjectDetail } from "../projects/ProjectDetail"
+import { TasksList } from "../tasks/TasksList"
 
-export const MakerProjectDashboard
+
+
+export const MakerProjectDashboard = (props) => {
+   return (
+        <>     
+            <UserDetail {...props} />
+            <ProjectDetail {...props} />
+            <TasksList {...props} />
+        </>
+   ) 
+
+}
