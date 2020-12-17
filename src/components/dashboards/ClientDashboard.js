3c0ad159f7f7721382f3e93258a32570378718f7
Kristen Steele <ksteelephoto@gmail.com>
2020-12-17 08:46:14 -0600

    import contexts and fetch data. find and filter needed data.

--- src/components/dashboards/ClientDashboard.js
+++ src/components/dashboards/ClientDashboard.js
@@ -1,19 +1,61 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useContext, useState } from "react"
 
-// necessary contexts: projects
-
+// necessary contexts: projects, projectTasks, users
+import { ProjectContext } from "../projects/ProjectsDataProvider"
+import { ProjectTaskContext } from "../tasks/ProjectTasksDataProvider"
+// necessary component
+import { ProjectDetail } from "../projects/ProjectDetail"
 
 // need project data and doughnut chart 
 // do i need to put the doughnut here and import projectTask data to use in doughnut?
 
 export const ClientDashboard = (props) => {
-    // do the magic here to get client dashboard using the props.match.params
-    useEffect(() => {
-        console.log("props for client dashboard", props)
+    // pulling in the contexts needed
+    const { projects, getProjects } = useContext(ProjectContext)
+    const { projectTasks, getProjectTasks } = useContext(ProjectTaskContext)
+
+    const [project, setProject] = useState({user: {}, seadekColor: {}, paintType: {}})
+    const [filteredProjectTasks, setFiltered] = useState([])
+    // projectTasks filtered by isComplete status
+    const [complete, setComplete] = useState([])
+    const [incomplete, setIncomplete] = useState([])
+
+    useEffect(()=> {
+        getProjects().then(getProjectTasks)
     }, [])
 
+    useEffect(() => {
+        const clientId = parseInt(props.match.params.userId)
+        const foundProject = projects.find(p => p.userId === clientId)
+        setProject(foundProject)
+    }, [projects])
+
+    useEffect(() => {
+        console.log("found proj", project)
+
+        const filteredProjectTasks = projectTasks.filter(obj => obj.projectId === project.id)
+        setFiltered(filteredProjectTasks)
+    }, [project])
+    
+    useEffect(() => {
+        const done = filteredProjectTasks.filter(task => task.isComplete === true)
+        setComplete(done)
+        const notDone = filteredProjectTasks.filter(task => task.isComplete === false)
+        setIncomplete(notDone)
+    }, [filteredProjectTasks])
+    
+    
+    // useEffect(() => {
+    //     // const numFinished = complete.length
+    //     console.log("complete", complete.length)
+    //     console.log("incomplete", incomplete.length)
+    // }, [complete])
+
     return (
-    <div>I'm a general client dashboard.</div>
+        <>
+            <h3>Welcome, {project.user.firstName}!</h3>
+            
+        </>
     )
 }
 
