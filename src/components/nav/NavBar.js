e27d8c40673e91341f4df74459456ea1c7ae7ad7
Kristen Steele <ksteelephoto@gmail.com>
2020-12-24 14:48:04 -0600

    import Navbar from react-bootstrap

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { Link, Route } from "react-router-dom"
 import { Logout } from "../auth/Logout"
-import "./NavBar.css"
+// React-Bootstrap Component imports
+import Navbar from "react-bootstrap/Navbar"
 
 export const NavBar = (props) => {
     return (
        <>
-        <ul className="navbar">
+
+
+        {/* <ul className="navbar">
                 <li className="navbar__item active">
                     <Link className="navbar__link" to="/">Projects</Link>
                 </li>
@@ -17,7 +20,7 @@ export const NavBar = (props) => {
                     <Link className="navbar__link" to="/dashboards">Client Views</Link>
                 </li>
             </ul>
-            <Route render={props => <Logout {...props} />} />
+            <Route render={props => <Logout {...props} />} /> */}
         </>
     )
 }
